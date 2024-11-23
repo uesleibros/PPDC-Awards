4767d1d0e2adc55acaf5f2be44a8a606011859f8
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2024-11-23 20:48:42 -0300

    agora sim

--- src/app/page.js
+++ src/app/page.js
@@ -35,7 +35,7 @@ export default function Home() {
     },
     {
       "id": "peedroproductions",
-      "title": "Peedro Productions",
+      "title": "Peedrostudios",
       "size": 30
     },
     {
