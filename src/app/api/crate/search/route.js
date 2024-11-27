d0a085da28ea35549e5e5b3507e25ec96294e707
Ueslei Paim <uesleibros@gmail.com>
2024-11-26 22:39:24 -0300

    Update route.js

--- src/app/api/crate/search/route.js
+++ src/app/api/crate/search/route.js
@@ -1,7 +1,7 @@
 export async function POST(request) {
   const { searchGame } = await request.json();
 
-  const req = await fetch(`https://pptgamespt.wixsite.com/crate/_functions/api/v3/projects?title=contains(${encodeURIComponent(searchGame)})`);
+  const req = await fetch(`https://pptgamespt.wixsite.com/crate/_functions/api/v3/projects?title=contains(${searchGame})`);
 
   if (req.ok) {
     const data = await req.json();
