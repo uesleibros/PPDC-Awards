1c082dee56a5b14c3d335b7e19e6ea4294454e3b
Ueslei Paim <uesleibros@gmail.com>
2024-12-19 01:17:56 -0300

    Update supabase-middleware.js

--- src/lib/supabase-middleware.js
+++ src/lib/supabase-middleware.js
@@ -1,30 +1,30 @@
 import { createServerClient } from "@supabase/ssr";
 import { NextResponse } from "next/server";
 
-export async function updateSession(request) {
-  // Inicializa a resposta de forma padrão
-  let supabaseResponse = NextResponse.next();
+export async function updateSession(request, requestHeaders) {
+  let supabaseResponse = NextResponse.next({
+    request: {
+      headers: requestHeaders
+    }
+  });
 
-  // Cria o cliente Supabase com as cookies do request
   const supabase = createServerClient(
     "https://doqpwhcglnhkftttcluc.supabase.co",
     process.env.NEXT_PUBLIC_SUPABASE_KEY,
     {
       cookies: {
         getAll() {
-          return request.cookies.getAll();  // Obtém todos os cookies
+          return request.cookies.getAll();
         },
         setAll(cookiesToSet) {
-          // Configura os cookies na resposta
           cookiesToSet.forEach(({ name, value, options }) => {
-            request.cookies.set(name, value, options);  // Set cookies on the request
-            supabaseResponse.cookies.set(name, value, options);  // Set cookies in the response
+            request.cookies.set(name, value, options);
+            supabaseResponse.cookies.set(name, value, options);
           });
         },
       },
     }
   );
 
-  // Retorna a resposta do Supabase, que inclui os cookies atualizados
   return supabaseResponse;
 }
