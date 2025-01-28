e1ce2126d2d94bf0e0749b1f16585d8c6c96a316
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2025-01-28 12:22:51 -0300

    corrigido bug

--- src/app/classificados.com/page.jsx
+++ src/app/classificados.com/page.jsx
@@ -14,7 +14,6 @@ export default async function ClassificadosFase1Debug() {
   const protocol = headersList.get("x-forwarded-proto");
   const host = headersList.get("host");
 	const authorizedList = ["764259870563631114", "808627555027910676", "616386370063302686", "480338857724739594", "532970259422904340"];
-	const supabase = await createClient();
 	const response = await fetch(`${protocol}://${host}/api/auth`, {
     method: "GET"
   });
