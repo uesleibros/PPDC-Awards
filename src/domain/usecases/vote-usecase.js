62006cce9a451887c2bcfc54d54940b379468df4
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2025-02-02 22:39:04 -0300

    novas features pro classificados.com2

--- src/domain/usecases/vote-usecase.js
+++ src/domain/usecases/vote-usecase.js
@@ -17,7 +17,7 @@ export async function vote(project_id, category_id, phase, author_id) {
   if (phase === "PHASE_2") {
     const alreadyLimitedQuotaPhase2 = await voteRepo.limitedVotesPhase2(category_id, author_id);
     if (alreadyLimitedQuotaPhase2)
-      throw new Error("VOcê só pode votar em até 2 jogos por categoria.");
+      throw new Error("Você só pode votar em até 2 jogos por categoria.");
   }
 
   await voteRepo.insertVote(project_id, category_id, phase, author_id);
